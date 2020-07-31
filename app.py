5bf6484af401a0d35b17607cc10d21e4f4ebf567
Rupam <sarrupam@gmail.com>
2020-07-31 11:21:24 +0530

    update

---
+++ app.py
@@ -0,0 +1,5 @@
+x = "Rupam Sarkar"
+for i in range(0, len(x), 2):
+    print(x[i])
+
+print(int(10 / 3))
--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 print('hello world 123')
+=======
+print("hello world")
+>>>>>>> Stashed changes
